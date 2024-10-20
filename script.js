4ffec9d6d1ccd6d16d183e55d82842813c73adee
Mani-TheDeveloper <manishankar.ams@gmail.com>
2024-10-20 15:38:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,261 @@
+const gemini = [
+    {
+        id: 1,
+        imgURL: 'https://sund-images.sunnxt.com/195490/1000x1000_GeminiTVHDDolbyVision_195490_11f32f56-3df0-486a-b6dd-1823661ffad2.jpg',
+        name: 'Gemini TV HD - Dolby Vision',
+        url: 'https://www.sunnxt.com/live/gemini-tv-hd-dolby-vision-telugu-live/195490/gemini-tv-hd-dolby-vision'
+    },
+    {
+        id: 2,
+        imgURL: 'https://sund-images.sunnxt.com/194392/1920x1080_GeminiTVHD_194392_f0f60f9d-f692-4ab4-b335-cb971893af2f.jpg',
+        name: 'Gemini Movies HD',
+        url: 'https://www.sunnxt.com/live/gemini-movies-hd-telugu-live/192537/gemini-movies-hd'
+    },
+    {
+        id: 3,
+        imgURL: 'https://sund-images.sunnxt.com/194393/640x360_GeminiMusicHD_194393_dbfc3b93-0581-4d7c-985b-c160e0031a73.jpg',
+        name: 'Gemini Music HD',
+        url: 'https://www.sunnxt.com/live/gemini-music-hd-telugu-live/194393/gemini-music-hd'
+    },
+    {
+        id: 4,
+        imgURL: 'https://sund-images.sunnxt.com/194394/1000x1000_GeminiComedy_194394_0fe7c915-5f33-4c6f-909b-3d8d45f92f67.png',
+        name: 'Gemini Comedy',
+        url: 'https://www.sunnxt.com/live/gemini-comedy-telugu-live/194394/gemini-comedy'
+    },
+    {
+        id: 5,
+        imgURL: 'https://sund-images.sunnxt.com/194337/1000x1000_GeminiLife_194337_e1145455-d580-4b95-8f35-73e48e199533.png',
+        name: 'Gemini Life',
+        url: 'https://www.sunnxt.com/live/gemini-life-telugu-live/194337/gemini-life'
+    },
+    {
+        id: 6,
+        imgURL: 'https://sund-images.sunnxt.com/194346/200x200_KushiTV_194346_21645eb7-43c4-4721-89bf-b2658d856251.png',
+        name: 'Kushi TV',
+        url: 'https://www.sunnxt.com/live/kushi-tv-telugu-live/194346/kushi-tv'
+    },
+];
+
+const zee = [
+    {
+        id: 7,
+        imgURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRRpTKMg1wI09G2IL9h5w7wIO0Dlw35Xnp9A&s',
+        name: 'Zee Telugu HD',
+        url: 'https://www.zee5.com/live-tv/zee-telugu-hd/0-9-zeetelugu'
+    },
+    {
+        id: 8,
+        imgURL: 'https://smartads.in/resources/assets/uploads/product_group/television/zee-cinemalu-hd.png',
+        name: 'Zee Cinemalu HD',
+        url: 'https://www.zee5.com/live-tv/zee-cinemalu-hd/0-9-zeecinemalu'
+    }
+];
+
+const spt = [
+    {
+        id: 9,
+        imgURL: 'https://yt3.googleusercontent.com/ytc/AIdro_kxsY7lAdRk1ZHKtVK7BtAZuCV_V8tVSVu7mynBS4Ix-BU=s176-c-k-c0x00ffffff-no-rj-mo',
+        name: 'Bhakthi TV',
+        url: 'https://www.youtube.com/watch?v=d0dB3kSCMmM'
+    },
+    {
+        id: 10,
+        imgURL: 'https://yt3.googleusercontent.com/ytc/AIdro_mfAUc7DTGQgkazQJiDcL764YmHHKo2gWxxLtAALOIJm4I=s160-c-k-c0x00ffffff-no-rj',
+        name: 'SVBC TTD',
+        url: 'https://www.youtube.com/watch?v=mTm4j5NWf3s'
+    },
+    {
+        id: 11,
+        imgURL: 'https://yt3.googleusercontent.com/jmhEx_E--j0f2Nj6gqlJU5jGQDA4B93JDZM0_REDNxk3VeF8QLz3n97vEK1Zs2o4KSaawda3ww=s160-c-k-c0x00ffffff-no-rj',
+        name: 'Hindu Dharmam',
+        url: 'https://www.youtube.com/watch?v=r-VKXUVmytU'
+    },
+    {
+        id: 12,
+        imgURL: 'https://jiotvimages.cdn.jio.com/dare_images/images/Aastha_Telugu.png',
+        name: 'Aastha Telugu',
+        url: 'https://www.zee5.com/live-tv/aastha-telugu/0-9-9z5398768'
+    }
+];
+
+const news = [
+    {
+        id: 13,
+        imgURL: 'https://yt3.googleusercontent.com/ytc/AIdro_kINV9J3l1Tq1kyefK7yNS0-IkKG3AONgiu6HmUc19JjMs=s160-c-k-c0x00ffffff-no-rj',
+        name: 'TV9 Telugu Live',
+        url: 'https://www.youtube.com/watch?v=II_m28Bm-iM'
+    },
+    {
+        id: 14,
+        imgURL: 'https://yt3.googleusercontent.com/u8QRZA82BCje9zgMhgDk_ROVb_zwcJz5iDJ-BQnNk47h6vY_PwqREBeJeUaVCCAZ3LAX_MDBTbg=s160-c-k-c0x00ffffff-no-rj',
+        name: 'ETV Andhra Pradesh',
+        url: 'https://www.youtube.com/watch?v=KvZIEgHfscU'
+    },
+    {
+        id: 15,
+        imgURL: 'https://yt3.googleusercontent.com/xLfD0MEsY-OM2Adud-KYaa3corga0dmHs2vahrBqCNFUPJq4pPYVO1W0xO_PQABCunuCxL6SJA=s160-c-k-c0x00ffffff-no-rj',
+        name: 'ABN Telugu',
+        url: 'https://www.youtube.com/watch?v=HoYsWagMFfE'
+    },
+    {
+        id: 16,
+        imgURL: 'https://yt3.googleusercontent.com/3eMz2ePLhVmre6X5oA_2Q6WOmYYW4xMaD2czPYtXC79JlK-sSAFJ7hiWp9Kx1cH7YMWmJi9Xw80=s160-c-k-c0x00ffffff-no-rj',
+        name: 'ETV Telangana',
+        url: 'https://www.youtube.com/watch?v=MHVeUcd7vbs'
+    },
+    {
+        id: 17,
+        imgURL: 'https://yt3.googleusercontent.com/ytc/AIdro_kl_HB7RxIxxf262dvKMtFAvjTbaeMTa2mgAjD6IXsZowF5=s160-c-k-c0x00ffffff-no-rj',
+        name: 'V6 News Telugu',
+        url: 'https://www.youtube.com/watch?v=_xtceKvlVYQ'
+    },
+    {
+        id: 18,
+        imgURL: 'https://yt3.googleusercontent.com/6TgxEDCRlNcbfNSQj7DtfyHFurPnYYlYCv-8ggyh4hXcclWOZ2otMSSHjWzZepcOimjIGGH8eA=s160-c-k-c0x00ffffff-no-rj',
+        name: '10TV News Telugu',
+        url: 'https://www.youtube.com/watch?v=byG7EGw9NPs'
+    },
+    {
+        id: 19,
+        imgURL: 'https://yt3.googleusercontent.com/ytc/AIdro_mRGOKibahsYRFGyVvIlwWZBEhpAhD2zkHZpydpLgjhPRA=s160-c-k-c0x00ffffff-no-rj',
+        name: 'NTV Telugu',
+        url: 'https://www.youtube.com/watch?v=L0RktSIM980'
+    },
+    {
+        id: 20,
+        imgURL: 'https://yt3.googleusercontent.com/93tHmOkT1G1qrPxtoASRHz8rmbBPXMkYTBPfQP2lI04QAr5FdQme5yZEl66L4ytnBDwfkmY26g=s160-c-k-c0x00ffffff-no-rj',
+        name: 'Zee Telugu News',
+        url: 'https://www.youtube.com/watch?v=ffI9-IioJK8'
+    },
+    {
+        id: 21,
+        imgURL: 'https://yt3.googleusercontent.com/ytc/AIdro_lxDNkVmsYCjbbDzTsVH42etOlCxG8mSMYVwvUkdAFdkwc=s160-c-k-c0x00ffffff-no-rj',
+        name: 'T News Telugu',
+        url: 'https://www.youtube.com/watch?v=N-4RyrSNDjU'
+    },
+    {
+        id: 22,
+        imgURL: 'https://yt3.googleusercontent.com/5ktLv4mpzjW65uokh-Rn_3jDQjwTcAYv4s-aU2LJadfeQLnhu3jem8o7hsFNBoWcIFtxZ7d8=s160-c-k-c0x00ffffff-no-rj',
+        name: 'Sakshi TV',
+        url: 'https://www.youtube.com/watch?v=izl4gpPIu-8'
+    },
+    {
+        id: 23,
+        imgURL: 'https://yt3.googleusercontent.com/jvfA4HOgSo7vWcxeCt5_8OY4IOIDUTue6tUfeUFv1nMF8aRzHNZZCs35-aMDBYh9gm4D7IQu=s160-c-k-c0x00ffffff-no-rj',
+        name: 'BIG TV Live',
+        url: 'https://www.youtube.com/watch?v=ueJsgNKeq8c'
+    },
+    {
+        id: 24,
+        imgURL: 'https://yt3.googleusercontent.com/Hz0ZWAZq64ouDXsGnGv9h1cG6wezsPuYowayytoEVchaYErA8k08WWfA5ox35jJ0hRgn4oBkHg=s160-c-k-c0x00ffffff-no-rj',
+        name: 'TV5 News',
+        url: 'https://www.youtube.com/watch?v=ho9JlBnrGWg'
+    },
+    {
+        id: 25,
+        imgURL: 'https://yt3.googleusercontent.com/UTRQE2BLwtLV8Z86Bn1YgOGACFne0YiyYNrQHP2gnO7cGb95aqG0OYo6Z4rMMFm9YzC9pTHIttM=s160-c-k-c0x00ffffff-no-rj',
+        name: 'hmtv News Live',
+        url: 'https://www.youtube.com/watch?v=3JodH4xoqJs'
+    },
+    {
+        id: 26,
+        imgURL: 'https://yt3.googleusercontent.com/mE_a0AGMUv3GQ2s89g3JuPrOr7vCJw_i2MeagLbqjo-WVHECino1FdV5EZpOk__KNCy5-hqEIA=s160-c-k-c0x00ffffff-no-rj',
+        name: 'Mahaa News',
+        url: 'https://www.youtube.com/watch?v=pSHX6tCPDC0'
+    },
+    {
+        id: 27,
+        imgURL: 'https://yt3.googleusercontent.com/W5cLN3UUH2bEHM8Q3t-h9G1vN5HY7J8fcqH0e-350RrMEtKF_vFNa4ZB4UJSQWZCFGz8OLKd-g=s160-c-k-c0x00ffffff-no-rj',
+        name: 'Prime9 News',
+        url: 'https://www.youtube.com/watch?v=4PpS_RGUzfQ'
+    }
+];
+
+const content = document.querySelector('#content');
+
+const handleHomeBtn = () => {
+    content.innerHTML = `
+    <div class="bg-white container-fluid" style='height: 50vh; border-radius: 2rem;' >
+      <div class="row h-100">
+        <div class="col-6 text-black d-flex flex-column justify-content-center px-5">
+          <h2 class="fw-bold">Apna Tv</h2>
+          <h4 class="text-decoration-underline mb-5">Apna Desh, Apna Channel</h4>
+          <h3 class="fw-bold">Our Features :</h3>
+          <ul>
+            <li class="fs-5 fw-bold">Live TV Streaming</li>
+            <li class="fs-5 fw-bold">Categories and Genres</li>
+            <li class="fs-5 fw-bold">Easy to Navigate</li>
+            <li class="fs-5 fw-bold">User-friendly Interface</li>
+          </ul>
+        </div>
+        <div class="col-6 text-center d-flex flex-column justify-content-center align-items-center">
+          <h2 class="fw-bold">Catch all channels at one place - "Apna TV"</h2>
+          <img src="https://toonz.co/wp-content/uploads/Ott_platforms.jpg"
+            alt="logo"
+            class="w-75 mt-1 h-75"
+            style='border-radius: 2rem;' />
+        </div>
+      </div>
+    </div>
+    `;
+};
+
+function Channel(channel) {
+    return (
+        `<div class="col-3 text-center mt-3">
+            <div class="card h-100 px-2 py-3 d-flex flex-column justify-content-between">
+                <img src=${channel.imgURL} alt="Channel Logo" loading="lazy" class="h-75" />
+                <h4 class="card-title mt-2">${channel.name}</h4>
+                <a href=${channel.url} target="_blank" class=" text-decoration-none text-white">
+                    <button class="btn btn-outline-primary w-75 fw-bold">Watch Now!!!</button>
+                </a>
+            </div>
+        </div>`
+    )
+};
+
+const handleEntBtn = () => {
+    content.innerHTML = `
+    <div>
+        <div class="gemini">
+            <h2 class="fw-bold text-decoration-underline text-white">Gemini :</h2>
+            <div class="row">
+            ${gemini.map((channel) => Channel(channel)).join('')}
+            </div>
+        </div>
+        <div class="zee mt-5">
+            <h2 class="fw-bold text-decoration-underline text-white">Zee :</h2>
+            <div class="row">
+                ${zee.map((channel) => Channel(channel)).join('')}
+            </div>
+        </div>
+    </div>
+    `;
+};
+
+const handleSptBtn = () => {
+    content.innerHTML = `
+    <div>
+        <div class="spt">
+            <h2 class="fw-bold text-decoration-underline text-white">Spiritual :</h2>
+            <div class="row">
+            ${spt.map((channel) => Channel(channel)).join('')}
+            </div>
+        </div>
+    </div>
+    `;
+};
+
+const handleNewsBtn = () => {
+    content.innerHTML = `
+    <div>
+        <div class="news">
+            <h2 class="fw-bold text-decoration-underline text-white">News :</h2>
+            <div class="row">
+            ${news.map((channel) => Channel(channel)).join('')}
+            </div>
+        </div>
+    </div>
+    `;
+}
